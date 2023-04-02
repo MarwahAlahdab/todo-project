a8517737168c571cfc6d13d63285f327a228d13e
marwasyr <marwaalahdab14@gmail.com>
2023-04-02 20:44:04 +0300

    js file added

--- JS/app.js
+++ JS/app.js
@@ -1,18 +1,17 @@
-// Ask the user to confirm if he wants to skip the welcoming message.
-// Alert a welcoming message with the name of the user and the title Mr if the user is male and Ms if the user is female, and if the gender input is not correct alert the welcoming message without the title, according to the confirmation.
-// Continue working on the styling of the project.
+
+
 
 
 "use strict";
 
 console.log("hi");
-prompt("Enter your name:");
+var username = prompt("Enter your name:");
 
 var gender = prompt("Enter your gender (Male/Female):");
 
-if (gender !== "male" || gender !== "female" ) {
+if (gender !== "Male" && gender !== "Female" ) {
 
-  alert("Invalid input. Please try again!");
+  alert("Invalid input!");
 }
 
 
@@ -21,6 +20,19 @@ var age = prompt("Enter your age:");
 if (age <= 0 ) {
 
   alert(" Your age is not valid!");
+}
+
+
+let message = confirm("Do you want to skip the welcoming message? ");
+
+if(message === false){
+
+    if (gender === "Female") {
+    alert("Welcome, Ms."  +username+" !");
+}else if(gender =="Male"){
+    alert("Welcome, Mr. " +username+" !");
+}
+
 }
 
 
