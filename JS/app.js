c1c53b5cb5033362204efa6ebc57f13e6b79b0a0
marwasyr <marwaalahdab14@gmail.com>
2023-04-02 17:29:23 +0300

    js file added

---
+++ JS/app.js
@@ -0,0 +1,14 @@
+// Ask the user to enter his/her name as input.
+// Ask the user to enter his/her gender as input. The answer should be either (male or female).
+// Ask the user to enter his/her age as input and alert the user if the age is less than or equal to zero.
+"use strict";
+
+console.log("hi");
+// prompt("Enter your name:");
+
+// let gender = prompt("Enter your gender (Male/Female):");
+
+// if (gender !== "male" || gender !== "female" ) {
+
+//   alert("Invalid input. Please try again!");
+// }
