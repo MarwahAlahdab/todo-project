46bc5a2e0ed737feaf2b0539a497c84c5118da4c
marwasyr <marwaalahdab14@gmail.com>
2023-04-02 17:41:28 +0300

    js file added

--- JS/app.js
+++ JS/app.js
@@ -1,14 +1,26 @@
-// Ask the user to enter his/her name as input.
-// Ask the user to enter his/her gender as input. The answer should be either (male or female).
-// Ask the user to enter his/her age as input and alert the user if the age is less than or equal to zero.
+// Ask the user to confirm if he wants to skip the welcoming message.
+// Alert a welcoming message with the name of the user and the title Mr if the user is male and Ms if the user is female, and if the gender input is not correct alert the welcoming message without the title, according to the confirmation.
+// Continue working on the styling of the project.
+
+
 "use strict";
 
 console.log("hi");
-// prompt("Enter your name:");
+prompt("Enter your name:");
 
-// let gender = prompt("Enter your gender (Male/Female):");
+var gender = prompt("Enter your gender (Male/Female):");
 
-// if (gender !== "male" || gender !== "female" ) {
+if (gender !== "male" || gender !== "female" ) {
 
-//   alert("Invalid input. Please try again!");
-// }
+  alert("Invalid input. Please try again!");
+}
+
+
+var age = prompt("Enter your age:");
+
+if (age <= 0 ) {
+
+  alert(" Your age is not valid!");
+}
+
+
