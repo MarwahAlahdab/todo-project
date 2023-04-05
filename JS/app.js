0868524433e20c82c371049a1dcf029966914321
marwasyr <marwaalahdab14@gmail.com>
2023-04-05 12:28:01 +0300

    update js file

--- JS/app.js
+++ JS/app.js
@@ -4,7 +4,7 @@
 
 "use strict";
 
-console.log("hi");
+
 var username = prompt("Enter your name:");
 
 var gender = prompt("Enter your gender (Male/Female):");
@@ -37,7 +37,42 @@ if(message === false){
 
 }
 
+// lab 6
 
 
 
 
+var questionOne = prompt("Are you an employee ? (Yes/No)");
+var questionTwo = prompt("Have you used a to-do list website before? (Yes/No)");
+var questionThree = prompt("Do you find it helpful? (Yes/No)");
+
+var arr =[];
+
+validation(questionOne, arr);
+console.log(arr[0]);
+validation(questionTwo, arr);
+
+validation(questionThree, arr);
+
+
+
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+
+
+
+
+
+
+function validation( question , array1 ){
+
+  if (question ==="" && question.toLowerCase() !== "yes" && question.toLowerCase() !== "no"  ) {
+
+    array1.push("invalid");
+  }else{
+   array1.push(question);
+  }
+
+}
